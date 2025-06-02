b804b0c84aa945c1d4406a72c9d39ba9ea5bd7c0
Timm49 <timvanengelen@gmail.com>
2025-06-02 23:35:12 +0200

    tiny refactor

--- src/Console/Commands/GenerateEmbeddingsCommand.php
+++ src/Console/Commands/GenerateEmbeddingsCommand.php
@@ -15,52 +15,60 @@ class GenerateEmbeddingsCommand extends Command
 
     public function handle(): int
     {
+        $errors = false;
+
         foreach (self::getRegisteredModels() as $modelClass) {
-
-            if (!class_exists($modelClass)) {
-                $this->warn("Model class $modelClass does not exist.");
-                continue;
+            if (!$this->handleModel($modelClass)) {
+                $errors = true;
             }
-
-            $modelInstance = new $modelClass;
-
-            if (!$modelInstance instanceof Model) {
-                $this->warn("$modelClass is not a valid Eloquent model.");
-                continue;
-            }
-
-            if ($this->option('force')) {
-                $records = $modelInstance::all();
-            } else {
-                $primaryKey = (new $modelInstance)->getKeyName();
-
-                $records = $modelInstance::whereNotIn($primaryKey, function ($query) use ($modelClass) {
-                    $query->select('embeddable_id')
-                        ->from('embeddings')
-                        ->where('embeddable_type', $modelClass);
-                })->get();
-            }
-
-            $count = $records->count();
-
-            if ($count === 0) {
-                $this->line("No records without embeddings found for model: {$modelClass}");
-                continue;
-            }
-
-            if (!$this->confirm("This will generate embeddings for {$count} records in {$modelClass}. Do you want to continue?", true)) {
-                $this->warn("Skipped model: {$modelClass}");
-                continue;
-            }
-
-            $this->info("Generating embeddings for $modelClass.");
-
-            $records->each(fn($record) => SimilarContent::createEmbedding($record));
-
-            $this->info("Embeddings for $modelClass generated.");
         }
 
-        return 0;
+        return $errors ? 0 : 1;
+    }
+
+    private function handleModel(mixed $modelClass): bool
+    {
+        if (!class_exists($modelClass)) {
+            $this->warn("Model class $modelClass does not exist.");
+            return false;
+        }
+
+        $modelInstance = new $modelClass;
+
+        if (!$modelInstance instanceof Model) {
+            $this->warn("$modelClass is not a valid Eloquent model.");
+            return false;
+        }
+
+        if ($this->option('force')) {
+            $records = $modelInstance::all();
+        } else {
+            $primaryKey = (new $modelInstance)->getKeyName();
+
+            $records = $modelInstance::whereNotIn($primaryKey, function ($query) use ($modelClass) {
+                $query->select('embeddable_id')
+                    ->from('embeddings')
+                    ->where('embeddable_type', $modelClass);
+            })->get();
+        }
+
+        $count = $records->count();
+
+        if ($count === 0) {
+            $this->line("No records without embeddings found for model: {$modelClass}");
+            return false;
+        }
+
+        if (!$this->confirm("This will generate embeddings for {$count} records in {$modelClass}. Do you want to continue?", true)) {
+            $this->warn("Skipped model: {$modelClass}");
+            return false;
+        }
+
+        $this->info("Generating embeddings for $modelClass.");
+
+        $records->each(fn($record) => SimilarContent::createEmbedding($record));
+
+        return true;
     }
 
     public static function getRegisteredModels(?string $path = null): array
@@ -71,11 +79,7 @@ class GenerateEmbeddingsCommand extends Command
         foreach (glob($path . '/*.php') as $file) {
             $className = self::extractNamespaceFromFile($file) . '\\' . basename($file, '.php');
 
-            if (! class_exists($className)) {
-                continue;
-            }
-
-            if (! is_subclass_of($className, Model::class)) {
+            if (! class_exists($className) || ! is_subclass_of($className, Model::class)) {
                 continue;
             }
 
